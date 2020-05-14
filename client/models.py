abbb03758d6018b3c12103874431d207bfe1978c
andi dwi purnama <andidepe11@gmail.com>
2020-05-14 08:24:02 +0700

    Revise client model

--- client/models.py
+++ client/models.py
@@ -3,14 +3,14 @@
 from django.db import models
 
 class ClientModel(models.Model):
-    name = models.CharField(max_length=155,unique = True)
-    street_name = models.CharField(max_length=100, null=True)
-    suburb = models.CharField(max_length=100, unique = True)
-    postcode = models.CharField(max_length=50,null=True)
-    state = models.CharField(max_length=100, null=True)
-    contact = models.CharField(max_length=100, null = True)
-    email = models.EmailField(max_length=155, unique = True)
-    phone_number = models.IntegerField(null=True)
+    name = models.CharField(max_length=155,unique=True, blank=False)
+    street_name = models.CharField(max_length=100, blank=True)
+    suburb = models.CharField(max_length=100, blank = True)
+    postcode = models.CharField(max_length=50,blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    contact = models.CharField(max_length=100, blank = True)
+    email = models.EmailField(max_length=155, unique = True, blank=False)
+    phone_number = models.CharField(max_length=13, blank=False)
 
     def __str__(self):
         return self.name
